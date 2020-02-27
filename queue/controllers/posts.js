f4d2ecd9855b3648629407c4d72d40dcc891b0e6
Youssef_Sawiris <youssef.sawiris@students.makeschool.com>
2020-02-26 18:05:55 -0800

    User can create a post

---
+++ queue/controllers/posts.js
@@ -0,0 +1,18 @@
+const Post = require('../models/post');
+
+
+module.exports = (app) => {
+
+    // CREATE
+    app.post('/posts/new', (req, res) => {
+        // INSTANTIATE INSTANCE OF POST MODEL
+        const post = new Post(req.body);
+
+        // SAVE INSTANCE OF POST MODEL TO DB
+        post.save((err, post) => {
+            // REDIRECT TO THE ROOT
+            return res.redirect(`/`);
+        })
+    });
+
+};
