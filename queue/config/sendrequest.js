c7243841375ed4eb552034869eeb5404f29fceee
Youssef_Sawiris <youssef.sawiris@students.makeschool.com>
2020-03-04 00:51:20 -0800

    user can request a song

--- queue/config/sendrequest.js
+++ queue/config/sendrequest.js
@@ -54,10 +54,11 @@ $(document).ready(function() {
         $('#reload').load(location.href + ' #reload');
     });
     $('#change_song').on('click', function() {
-        let spotifytoken = 'BQCV3CX9ibHFbYsaSsp4-1uKX4vjaTefIhhdXuK4Nb5483W6wMuVFjJ9iWA6reowyta_CBMNKEbsVs_zNqyLuBFkewJupPWpG8fl9ZAckaCDAn7QxtKvEV3ql-lvziB7RI7qno_s6kHsn7QoypDTk__-DbzP7dc4'
+        let spotifytoken = $('#user_token').val()
+        let song = $('#user_song').val()
 
         $.ajax({
-            url: 'https://api.spotify.com/v1/me/player/queue?uri=spotify%3Atrack%3A0W9E3s2G4szLUwXsE17x5E',
+            url: 'https://api.spotify.com/v1/me/player/queue?uri=' + song,
             type: 'POST',
             headers: {
                 'Accept': 'application/json',
